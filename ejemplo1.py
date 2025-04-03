9d7e04feaebbb2fc9706e89623969046d58e29c5
Azael <azaelmart1010@gmail.com>
2025-04-03 10:51:25 -0600

    Create ejemplo1.py

---
+++ ejemplo1.py
@@ -0,0 +1,24 @@
+"""
+Esto es un comentario
+de varias
+lineas
+"""
+a = 3 #esto es un comentario de una sola linea
+b = 4
+c = a + b
+print("La suma de {} y {}".format(a,b,c))
+print(type(a))
+print(type(b))
+a = "Hola Mundo"
+print(a)
+print(type(a))
+
+a = input("Ingresa un dato: ")
+print(a)
+print(type(a))
+
+a = int(input("Ingresa un entero: "))
+print(type(a))
+
+a = float(input("Ingresa un entero: "))
+print(type(a))
