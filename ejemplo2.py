0df10f5b00c80f111220cf1149e1b74dd85542b8
Azael <azaelmart1010@gmail.com>
2025-04-03 10:51:42 -0600

    Create ejemplo2.py

---
+++ ejemplo2.py
@@ -0,0 +1,7 @@
+a = int(input("Ingresa el primer numero: "))
+b = int(input("Ingresa el segundo numero: "))
+
+if a > b:
+    print("{} es mayor que {}".format(a, b))
+else:
+    print("{} es mayor que {}".format(b, a))
